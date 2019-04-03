a060e52a56620e0223d51956af9943f1c980a7e7
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 23:55:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ class App < Sinatra::Base
 
   post '/' do
     @analyzed_text = TextAnalyzer.new(params["user_text"])
-binding.pry
     @array = @analyzed_text.text.gsub(/[^a-z]/, '').chars.group_by(&:itself).map { |letter, list| [letter, list.count]}.max_by(&:last)
     erb :results
   end
